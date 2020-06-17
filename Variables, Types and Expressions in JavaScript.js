9bc97d5351ea9e6ee9956b6619b3372607a853ea
McDagles <65178811+McDagles@users.noreply.github.com>
2020-06-17 20:11:54 +0200

    Create Variables, Types and Expressions in JavaScript.js

---
+++ Variables, Types and Expressions in JavaScript.js
@@ -0,0 +1,36 @@
+age = 2020 - 1990
+ww2 = 1945 - 1939
+tesla = 994 * 75
+secondsOfDay = 60 * 60 * 24
+hungarians = 14000000 / 7500000000
+bigger = 9600000 / 78900
+participants = 654 % 7
+
+
+
+let names = ["birs", "barack", "szilva", "vegyes"]
+
+names[2].length
+
+if (names[2].lenght < 5) {
+    alert ("kevesebb mint 5 betű");
+    } else {
+    alert ("több mint 5 betű");
+    }
+
+names[4] = "körte"
+
+names[0] = "Balázs"
+
+if (names < 4) {
+    alert ("kevesebb mint 4 elemből áll a lista");
+    } else {
+    alert ("több mint 4 elemből áll a lista");
+    }
+
+
+let rainbow = {
+     colors: ["red", "orange", "yellow", "green", "blue", "violet"],
+};
+
+rainbow.hasManyColors = rainbow.colors.length > 3;
